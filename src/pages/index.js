9728aa47ddcf79efdc6d5ca6937508903b74cc00
Kheoh Yee Wei <kwei88@gmail.com>
2018-04-05 22:17:04 +0800

    early beta user sign up form

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,21 @@ const IndexPage = ({ data }) => (
             A place that effectively keeps your costs and taxes to an absolute
             minimum to guarantee your fair share of stock market returns.
           </p>
+          <div>
+            <p>Subscribe below to get early access to the beta.</p>
+            <form className="flex-row prelaunch-form" autoComplete="off">
+              <input
+                type="email"
+                className="prelaunch-form__email"
+                name="email"
+                required
+                placeholder="Enter your email"
+              />
+              <button className="prelaunch-form__submit-btn" type="submit">
+                Get Notified
+              </button>
+            </form>
+          </div>
         </div>
         <div className="g-c6 g-c6--md">
           <Img
