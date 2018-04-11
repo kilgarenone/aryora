d76b8bd9ac4c1993b2d926dbb72680ea7a905e8f
Kheoh Yee Wei <kwei88@gmail.com>
2018-04-11 13:54:32 +0800

    add runtime

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,6 +9,9 @@
 exports.modifyBabelrc = ({ babelrc }) => ({
   ...babelrc,
   ...(process.env.NODE_ENV !== 'development' && {
-    plugins: babelrc.plugins.concat(['transform-regenerator']),
+    plugins: babelrc.plugins.concat([
+      'transform-regenerator',
+      'transform-runtime',
+    ]),
   }),
 })
