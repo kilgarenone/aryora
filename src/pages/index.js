e804807a0eb9bb365af95e022f51e6670ff156e3
Kheoh Yee Wei <kwei88@gmail.com>
2018-04-18 21:48:26 +0800

    words

--- src/pages/index.js
+++ src/pages/index.js
@@ -288,7 +288,7 @@ class IndexPage extends Component {
               </p>
             </div>
             <div>
-              <h4>Time-tested Principles</h4>
+              <h4>Reduce Risk</h4>
               <p>
                 Asset allocation. Low cost index funds. Buy-and-hold. Stay the
                 course. Don't panic.
