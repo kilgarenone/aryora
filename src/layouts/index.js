659cadcda116811f05780ba3cc29bc6aa49fba3d
Kheoh Yee Wei <kwei88@gmail.com>
2018-04-27 16:10:41 +0800

    Add open graphs meta contents

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,10 +8,26 @@ import './index.css'
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Aryora - Passive Investing"
+      title="Aryora - Passive Investing for Everyone"
       meta={[
-        { name: 'description', content: 'Low-cost and tax-smart passive investing ' },
-        { name: 'keywords', content: 'investing, index funds, passive investing' },
+        {
+          name: 'description',
+          content: 'Low-cost and tax-smart passive investing ',
+        },
+        {
+          name: 'keywords',
+          content: 'investing, index funds, passive investing',
+        },
+        {
+          property: 'og:title',
+          content: 'Aryora - Passive Investing for Everyone',
+        },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:url', content: 'https://aryora.com' },
+        {
+          property: 'og:image',
+          content: 'https://source.unsplash.com/DWGAlCVnDm0/1200x1200',
+        },
       ]}
     />
     <Header>Aryora</Header>
