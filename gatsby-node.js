ce16ec4139f830df5487bcf06533ee7d8269efb2
Kheoh Yee Wei <kwei88@gmail.com>
2018-04-30 17:57:25 +0800

    Enable async to generator transformer during development too

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,10 +8,16 @@
 
 exports.modifyBabelrc = ({ babelrc }) => ({
   ...babelrc,
-  ...(process.env.NODE_ENV !== 'development' && {
+  ...{
     plugins: babelrc.plugins.concat([
       'transform-regenerator',
       'transform-runtime',
     ]),
-  }),
-})
+  },
+});
+// ...(process.env.NODE_ENV !== 'development' && {
+//   plugins: babelrc.plugins.concat([
+//     'transform-regenerator',
+//     'transform-runtime',
+//   ]),
+// }),
