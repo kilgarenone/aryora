7da69f3597999ef48b4f6950b7392ac89cb749c7
Kheoh Yee Wei <kwei88@gmail.com>
2018-05-12 11:58:30 +0800

    http util funcs to check for http err codes and parse res json

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,1 +1,14 @@
-export const numberWithCommas = (x) => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+export const numberWithCommas = x => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+export function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  }
+  const error = new Error(response.statusText);
+  error.response = response;
+  throw error;
+}
+
+export function parseJSON(response) {
+  return response.json();
+}
