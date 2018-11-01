409a4a0b7292aff66ef6d28c743cf4bc24f0b82b
Kheoh Yee Wei <kwei88@gmail.com>
2018-11-01 14:40:50 +0800

    Double quotes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,22 @@
 module.exports = {
-  parser: 'babel-eslint',
-  extends: ['airbnb', 'prettier/react', 'plugin:prettier/recommended'],
+  parser: "babel-eslint",
+  extends: ["airbnb", "plugin:prettier/recommended", "prettier/react"],
   rules: {
-    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
-    'react/prop-types': 'off',
-    'react/destructuring-assignment': 'off',
-    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
-    'jsx-a11y/anchor-is-valid': [
-      'error',
+    "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
+    "react/prop-types": "off",
+    "react/destructuring-assignment": "off",
+    "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx"] }],
+    "jsx-a11y/anchor-is-valid": [
+      "error",
       {
-        components: ['Link'],
-        specialLink: ['to'],
-        aspects: ['noHref', 'invalidHref', 'preferButton']
+        components: ["Link"],
+        specialLink: ["to"],
+        aspects: ["noHref", "invalidHref", "preferButton"]
       }
     ]
   },
   settings: {
-    'import/core-modules': ['gatsby']
+    "import/core-modules": ["gatsby"]
   },
   env: {
     browser: true,
