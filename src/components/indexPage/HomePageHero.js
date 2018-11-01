32f65041ccf35715789d84a8290a36d991aa1b73
Kheoh Yee Wei <kwei88@gmail.com>
2018-11-01 18:59:31 +0800

    wip

--- src/components/indexPage/HomePageHero.js
+++ src/components/indexPage/HomePageHero.js
@@ -11,15 +11,32 @@ const styles = css`
   &::before {
     content: "";
     position: absolute;
-    left: 0;
-    bottom: -200px;
+    left: -47px;
+    bottom: -250px;
     height: 600px;
     width: 500px;
     border-radius: 19% 71% 38% 36% / 38% 44% 56% 27%;
     background: rgb(232, 243, 236);
-    background: radial-gradient(
-      circle,
+    background: linear-gradient(
+      107deg,
       rgba(232, 243, 236, 1) 0%,
+      rgba(250, 250, 250, 1) 66%
+    );
+  }
+
+  &::after {
+    content: "";
+    z-index: -1;
+    position: absolute;
+    right: -50px;
+    top: -76px;
+    height: 700px;
+    width: 600px;
+    border-radius: 73% 28% 14% 44% / 40% 34% 43% 72%;
+    background: rgb(252, 231, 221);
+    background: linear-gradient(
+      236deg,
+      rgba(252, 231, 221, 1) 0%,
       rgba(250, 250, 250, 1) 61%
     );
   }
@@ -36,7 +53,7 @@ const styles = css`
 
 function HomePageHero() {
   return (
-    <section className={styles}>
+    <section className={`${styles} g-p-t-3`}>
       <div className="container">
         <div className="hero">
           <h1 className="g-font-serif g-mb-3">
