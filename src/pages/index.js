f5df14962dc325e1f1b49d4a7b891a0a5fec92de
Kheoh Yee Wei <kwei88@gmail.com>
2018-11-12 17:58:04 +0800

    Adjust max-width for gatsby-image and improt globalStyles.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import HomePageHero from "../components/indexPage/HomePageHero";
 import Headline from "../components/indexPage/Headline";
 import HowItWorks from "../components/indexPage/HowItWorks";
 import JoinUsAction from "../components/indexPage/JoinUsAction";
+import "../css/globalStyles";
 
 class IndexPage extends Component {
   state = {};
@@ -31,14 +32,14 @@ export const query = graphql`
   query {
     headlineImg: file(relativePath: { eq: "headline.png" }) {
       childImageSharp {
-        fluid(maxWidth: 1000) {
+        fluid(maxWidth: 1400) {
           ...GatsbyImageSharpFluid_withWebp_tracedSVG
         }
       }
     }
     joinUsActionImg: file(relativePath: { eq: "peekBackground.png" }) {
       childImageSharp {
-        fluid(maxWidth: 1000) {
+        fluid(maxWidth: 1400) {
           ...GatsbyImageSharpFluid_withWebp_tracedSVG
         }
       }
