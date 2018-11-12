f5e8be450a738b73ee57f1adf0a2d299ff503a60
Kheoh Yee Wei <kwei88@gmail.com>
2018-11-12 17:54:00 +0800

    Position relative <Header />

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,24 @@
 import React from "react";
 import { css } from "react-emotion";
+import Heading from "./Heading";
 
 const Header = () => (
-  <header
-    className={css`
-      /* position: fixed;
-      top: 0;
-      left: 0;
-      right: 0;
-      background-color: #fff; */
-      /* padding-top: 2rem; */
-      /* z-index: 100; */
-    `}
-  >
+  <header>
     <div
       className={css`
         padding: 0.5rem 1.0875rem 0;
-        /* background-color: #fff1e5; */
       `}
     >
-      <h4
+      <Heading
+        tag="h3"
+        isSerifFont
         className={css`
-          font-family: "Times New Roman", Times, serif;
-          font-size: 1.8rem;
-          color: rgba(0, 0, 0, 0.67);
+          color: rgba(0, 0, 0, 0.6);
           letter-spacing: 0.01rem;
         `}
       >
         towernest
-      </h4>
+      </Heading>
     </div>
   </header>
 );
